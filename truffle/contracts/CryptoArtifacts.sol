063fd1e479980ffbf9c3ecf2943d49afaab216bf
arispen <arispen@gmail.com>
2018-08-01 13:16:33 +0200

    distributed random, list of players

--- truffle/contracts/CryptoArtifacts.sol
+++ truffle/contracts/CryptoArtifacts.sol
@@ -5,12 +5,9 @@ import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
 
-    uint currentSet = 0;
+    uint set = 0;
     uint numberOfSlots = 9;
     uint lootboxesLeft = 10000;
-    uint maxPower = 4;
-    uint maxBonus = 3;
-    
     uint packPrice = 500 szabo;
 
     struct Artifact {
@@ -24,30 +21,59 @@ contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
 
     mapping (address => uint[]) equipped;
     
+    mapping (address => bool) existingPlayers;
+    address[] listOfPlayers;
+    
     function random(uint64 upper) public returns (uint64 randomNumber) {
         uint _seed = lootboxesLeft;
         _seed = uint64(sha3(sha3(blockhash(block.number), _seed), now));
         return _seed % upper;
+    }
+    
+    function k100() returns (uint) {
+        return random(100);
+    }
+    
+    // returns 0,1,2,3
+    // 1%, 4%, 25%, 70%
+    function distributed4() returns (uint) {
+        uint k100 = k100();
+        if(k100 <= 1){
+            return 3;
+        } else if (k100 <= 5) {
+            return 2;
+        } else if (k100 <= 25) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    function updateListOfPlayers() {
+        require(existingPlayers[msg.sender] == false);
+        listOfPlayers.push(msg.sender);
+        existingPlayers[msg.sender] == true;
     }
 
     function equip(uint _artifactId) public {
         require(ownerOf(_artifactId) == msg.sender);
         uint _slot = artifacts[_artifactId].slot;
         equipped[msg.sender][_slot] = _artifactId;
+        updateListOfPlayers();
     }
 
-    function updateGame(uint _currentSet, uint _numberOfSlots, uint _lootboxesLeft) onlyOwner public {
-        require(_currentSet > currentSet);
-        currentSet = _currentSet;
+    function updateGame(uint _set, uint _numberOfSlots, uint _lootboxesLeft) onlyOwner public {
+        require(_set > set);
+        set = _set;
         numberOfSlots = _numberOfSlots;
         lootboxesLeft = _lootboxesLeft;
     }
     
-    function getCurrentPrice() returns (uint) {
+    function getCurrentPrice() public view returns (uint) {
         return lootboxesLeft.mul(lootboxesLeft).mul(2000000000000000).div(1000000);
     }
     
-    function updatePrice() {
+    function updatePrice() private {
         packPrice = getCurrentPrice();
     }
 
@@ -61,7 +87,7 @@ contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
         }
     }
     
-    function openOneLootbox() {
+    function openOneLootbox() private {
         lootboxesLeft = lootboxesLeft.sub(1);
         updatePrice();
         uint _id = allTokens.length.add(1);
@@ -74,7 +100,7 @@ contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
             + "-" + artifacts[_id].bonus );
     }
     
-    function calculateOpenPrice(_number) returns (uint) {
+    function calculateOpenPrice(_number) private view returns (uint) {
         // 1 usd = 2000000000000000 wei
         uint total = 0;
         for (uint i = 0; i < _number; i++) {
@@ -83,10 +109,10 @@ contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
         return total;
     }
     
-    function generateArtifact() returns (Artifact) {
+    function generateArtifact() private returns (Artifact) {
         uint _slot = random(numberOfSlots);
-        uint _power = random(maxPower);
-        uint _bonus = random(maxPower - 1);
+        uint _power = distributed4() + 1; // 1,2,3,4
+        uint _bonus = distributed4(); // 0,1,2,3
         return Artifact(currentSet, _slot, _power, _bonus);
     }
 
