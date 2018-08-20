42b8f10c11be37d26acdfc3346e8c8fd2445100e
arispen <arispen@gmail.com>
2018-08-20 17:47:17 +0200

    vue methods

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
+const SET = 0;
+
 const app = new Vue({
     el: '#app',
     data: {
         hello: 'Hello Vue!',
         items: [{
-            data: LOOT[22 - 1],
-            img: 22
+            slot: 0,
+            type: 0,
+            bonus: 0
         }, {
-            data: LOOT[26 - 1],
-            img: 26
+            slot: 1,
+            type: 1,
+            bonus: 1
         }],
+        equipped: []
+    }, 
+    methods: {
+        getPower: (type, bonus) => {
+            return type + 1 + bonus;
+        },
+        getName: (slot, type, bonus) => {
+            const bonusText = (bonus > 0) ? (' +' + bonus) : '';
+            return LOOT.sets[SET].slots[slot][type].name + bonusText;
+        },
+        getDescription: (slot, type) => {
+            return LOOT.sets[SET].slots[slot][type].description;
+        },
+        equip: function (item) {
+            this.equipped[item.slot] = item;
+        }
     }
 })
