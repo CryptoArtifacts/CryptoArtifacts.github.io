394ee4143e524fcdd97d3289bcfb0dc12d0a0b23
arispen <arispen@gmail.com>
2018-08-21 13:31:02 +0200

    simplified contract and struct, events, tokenURI

--- truffle/contracts/CryptoArtifacts.sol
+++ truffle/contracts/CryptoArtifacts.sol
@@ -5,17 +5,15 @@ import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
 
-    uint public set = 0;
-    uint public numberOfSlots = 9;
+    uint constant public initialLootboxes = 10000;
+    uint constant public numberOfSlots = 9;
+
     uint public lootboxesLeft = 10000;
     uint public packPrice = 0;
-    uint public initialLootboxes = 10000;
 
     struct Artifact {
-        uint set;
         uint slot;
-        uint power; // aka type
-        uint bonus; // aka variant
+        uint power; // aka rarity
     }
 
     mapping (uint => Artifact) public artifacts;
@@ -25,52 +23,45 @@ contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
     mapping (address => bool) public existingPlayers;
     address[] public listOfPlayers;
     
-    event LootboxesOpened();
-    event ArtifactEquipped();
-    event NewPlayer();
-    event GameUpdated();
+    event LootboxesOpened(uint number, address by);
+    event ArtifactEquipped(Artifact artifact, address by);
+    event NewPlayer(address playerAddress);
     
     function random(uint upper) public view returns (uint) {
-        return uint(blockhash((block.number - 1) * lootboxesLeft * listOfPlayers.length * block.timestamp)) % upper + 1;
+        return uint(blockhash(block.number-1 * lootboxesLeft * listOfPlayers.length * block.timestamp)) % upper + 1;
     }
     
     function k100() private view returns (uint) {
         return random(100);
     }
     
-    // returns 0,1,2,3
+    // returns 1000,100,10,1
     // 1%, 4%, 25%, 70%
-    function distributed4() private view returns (uint) {
+    function rollPower() private view returns (uint) {
         uint k100roll = k100();
         if(k100roll <= 1){
-            return 3;
+            return 1000;
         } else if (k100roll <= 5) {
-            return 2;
+            return 100;
         } else if (k100roll <= 25) {
-            return 1;
+            return 10;
         } else {
-            return 0;
+            return 1;
         }
     }
     
     function updateListOfPlayers() private {
-        require(existingPlayers[msg.sender] == false);
+        require(existingPlayers[msg.sender] == false, "added player must be new");
         listOfPlayers.push(msg.sender);
         existingPlayers[msg.sender] == true;
+        emit NewPlayer(msg.sender);
     }
 
     function equip(uint _artifactId) public {
-        require(ownerOf(_artifactId) == msg.sender);
+        require(ownerOf(_artifactId) == msg.sender, "only owner can equip item");
         uint _slot = artifacts[_artifactId].slot;
         equipped[msg.sender][_slot] = _artifactId;
-    }
-
-    function updateGame(uint _set, uint _numberOfSlots, uint _lootboxesLeft) onlyOwner public {
-        require(_set > set);
-        set = _set;
-        numberOfSlots = _numberOfSlots;
-        lootboxesLeft = _lootboxesLeft;
-        initialLootboxes = _lootboxesLeft;
+        emit ArtifactEquipped(artifacts[_artifactId], msg.sender);
     }
     
     function getCurrentPrice() public view returns (uint) {
@@ -83,42 +74,87 @@ contract CryptoArtifacts is ERC721Token("CryptoArtifacts", "CA"), Ownable {
     }
 
     function openLootboxes(uint _number) public payable {
-        require(lootboxesLeft >= _number);
-        require(_number >= 1);
-        // no matter how many player buys, we always get the last price
-        require(msg.value >= _number.mul(getCurrentPrice()));
+        require(lootboxesLeft >= _number, "no lootboxes left");
+        require(_number >= 1, "number of lootboxes to open must be 1 or more");
+        require(msg.value >= calculateOpenPrice(_number), "invalid price");
         
         for (uint i = 0; i < _number; i++) {
             openOneLootbox();
         }
         
         updateListOfPlayers();
-        updatePrice();
+        emit LootboxesOpened(_number, msg.sender);
     }
     
     function openOneLootbox() private {
         lootboxesLeft = lootboxesLeft.sub(1);
+        updatePrice();
         uint _id = allTokens.length.add(1);
         _mint(msg.sender, _id);
         artifacts[_id] = generateArtifact();
-        _setTokenURI(
-            _id,
-            string(
-                abi.encodePacked(
-                "https://cryptoartifacts.co/artifactimages/",
-                artifacts[_id].set,
-                artifacts[_id].slot,
-                artifacts[_id].power,
-                artifacts[_id].bonus
-            ))
-        );
+        setTokenURI(_id, artifacts[_id].slot, artifacts[_id].power);
+    }
+    
+    function calculateOpenPrice(uint _numberOfLootboxes) private view returns (uint) {
+        // 1 usd = 2000000000000000 wei
+        uint total = 0;
+        for (uint i = 0; i < _numberOfLootboxes; i++) {
+            total.add(getCurrentPrice());
+        }
+        return total;
     }
     
     function generateArtifact() private view returns (Artifact) {
         uint _slot = random(numberOfSlots);
-        uint _power = distributed4() + 1; // 1,2,3,4
-        uint _bonus = distributed4(); // 0,1,2,3
-        return Artifact(set, _slot, _power, _bonus);
+        uint _power = rollPower();
+        return Artifact(_slot, _power);
+    }
+
+    function uint2str(uint _i) internal pure returns (string){
+        if (_i == 0) return "0";
+        uint j = _i;
+        uint i = _i;
+        uint length;
+        while (j != 0){
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint k = length - 1;
+        while (i != 0){
+            bstr[k--] = byte(48 + i % 10);
+            i /= 10;
+        }
+        return string(bstr);
+    }
+
+    function strConcat(string _a, string _b, string _c, string _d, string _e) internal pure returns (string){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory _bc = bytes(_c);
+        bytes memory _bd = bytes(_d);
+        bytes memory _be = bytes(_e);
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
+        bytes memory babcde = bytes(abcde);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
+        return string(babcde);
+    }
+
+    function setTokenURI(uint id, uint slot, uint power) private {
+        uint uriId = initialLootboxes.mul(slot).add(power);
+
+        string memory url_1 = "https://cryptoartifacts.co/artifactimages/";
+        string memory url_2 = uint2str(uriId);
+
+        _setTokenURI(
+            id, 
+            strConcat(url_1, url_2, "", "", "")
+        );
     }
 
 }
