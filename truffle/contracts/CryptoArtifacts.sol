38c14c3e480b23406f44a0bb1dae4917ed6002ab
arispen <arispen@gmail.com>
2021-08-10 11:40:25 +0200

    Update CryptoArtifacts.sol

--- truffle/contracts/CryptoArtifacts.sol
+++ truffle/contracts/CryptoArtifacts.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity 0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/payment/PullPayment.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.2/contracts/security/PullPayment.sol";
 
 
-contract CryptoArtifacts is ERC721, Ownable, PullPayment {
+contract CryptoArtifacts is ERC721Enumerable, ERC721URIStorage,  Ownable, PullPayment {
     
     uint constant initialLootboxes = 21000;
     uint public lootboxesLeft = initialLootboxes;
@@ -16,10 +18,10 @@ contract CryptoArtifacts is ERC721, Ownable, PullPayment {
     
     event LootboxOpened(address by, uint tokenId, uint artifactId);
     
-    constructor() ERC721("CryptoArtifacts", "CA") public { }
+    constructor() ERC721("CryptoArtifacts", "CA") { }
     
     function random(uint lower, uint upper) private view returns (uint) {
-        return (uint(keccak256(abi.encodePacked(now, msg.sender, lootboxesLeft))) % (upper-lower)) + lower;
+        return (uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, lootboxesLeft))) % (upper-lower)) + lower;
     }
     
     // returns 1-80
@@ -38,39 +40,47 @@ contract CryptoArtifacts is ERC721, Ownable, PullPayment {
     }
     
     function getCurrentPrice() public view returns (uint) {
-        uint lootboxesSold = initialLootboxes.sub(lootboxesLeft);
-        return lootboxesSold.mul(lootboxesSold).div(1000000000).div(1 ether);
+        uint lootboxesSold = initialLootboxes - lootboxesLeft;
+        return lootboxesSold * lootboxesSold / 1000000000 / 1 ether;
     }
     
-    function toString(uint _base) internal pure returns (string memory) {
-        bytes memory _tmp = new bytes(32);
-        uint i;
-        for(i = 0;_base > 0;i++) {
-            _tmp[i] = bytes1((uint8(_base) % 10) + 48);
-            _base /= 10;
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
         }
-        bytes memory _real = new bytes(i--);
-        for(uint j = 0; j < _real.length; j++) {
-            _real[j] = _tmp[i--];
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
         }
-        return string(_real);
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 
     function openLootbox() public payable {
         require(lootboxesLeft >= 1, "no lootboxes left");
         require(msg.value >= getCurrentPrice(), "invalid price");
         
-        lootboxesLeft = lootboxesLeft.sub(1);
+        lootboxesLeft = lootboxesLeft - 1;
         uint _id = totalSupply();
         artifacts[_id] = rollArtifactId();
         _mint(msg.sender, _id);
-        _setTokenURI(_id, toString(artifacts[_id]));
+        _setTokenURI(_id, uint2str(artifacts[_id]));
 
         emit LootboxOpened(msg.sender, _id, artifacts[_id]);
     }
     
-    function setBaseURI(string memory baseURI) public onlyOwner {
-        _setBaseURI(baseURI);
+    function _baseURI() override internal pure returns (string memory) {
+        return "https://cryptoartifacts.co/";
     }
     
     function withdraw(uint amount) public onlyOwner returns(bool) {
@@ -78,6 +88,26 @@ contract CryptoArtifacts is ERC721, Ownable, PullPayment {
         address payable owner = payable(owner());
         _asyncTransfer(owner, amount);
         return true;
+    }
+    
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+    
+    function _burn(uint256 tokenId) internal virtual override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+    
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
 }
